6db5f8f13762769fb88d83c70a7d853f21f41161
Anton Usoltsev <usoltsevjr@gmail.com>
2020-07-07 14:27:21 +0300

    First commit

---
+++ src/Precision.java
@@ -0,0 +1,8 @@
+public class Precision {
+    public static void main(String[] args) {
+        double regularBonus = 0.3;
+        double specialBonus = 0.6;
+        double totalBonus = regularBonus + specialBonus;
+        System.out.println(totalBonus);
+    }
+}
